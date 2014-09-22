6ccca7be784fb2e3086385a031a347a5bb60ae2c
Piero Aiello <p.aiello27@outlook.it>
2014-09-22 22:48:43 +0200

    Update core.php

--- core.php
+++ core.php
@@ -1,8 +1,9 @@
 <?php
 /*
- * senza nome.php
+ * mlPaste 
+ * core.php
  * 
- * Copyright 2014 Piero <Piero@PIERO-PC>
+ * Copyright 2014 Piero Aiello <p.aiello27@outlook.it>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
